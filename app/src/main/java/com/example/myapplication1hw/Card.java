7f2416cdfb3600f3520b2e6c2386b2f7ba12b312
OfirCohen26 <Ofirco26@gmail.com>
2020-11-13 15:15:50 +0200

    v1

--- app/src/main/java/com/example/myapplication1hw/Card.java
+++ app/src/main/java/com/example/myapplication1hw/Card.java
@@ -9,10 +9,10 @@ import java.util.Random;
 
 public class Card {
 
+    final int minNumOfCard = 1;
+    final int maNumOxfCard = 13;
     private static Color[] colors = {Color.BLACK, Color.RED};
     private static Shape[] shapes = {Shape.DIAMOND, Shape.HEART, Shape.CLUBS, Shape.LEAF};
-    final int min = 1;
-    final int max = 13;
     private int value;
     private String color;
     private String shape;
@@ -23,7 +23,7 @@ public class Card {
     }
 
     private void raffleRandomValues(Random random) {
-        this.value = random.nextInt((max - min) + 1) + min;
+        this.value = random.nextInt((maNumOxfCard - minNumOfCard) + 1) + minNumOfCard;
         this.color = colors[random.nextInt(1 - 0) + 1 + 0].name();
         this.shape = shapes[random.nextInt(3-0) + 1 + 0].name();
     }
@@ -39,6 +39,18 @@ public class Card {
 
     public String getShape(){
         return this.shape;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
 
