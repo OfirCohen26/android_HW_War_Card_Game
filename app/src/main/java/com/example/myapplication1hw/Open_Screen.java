fdca4722845a94ee8a15be67e003b71e133a6f41
OfirCohen26 <Ofirco26@gmail.com>
2020-11-27 15:34:15 +0200

    v2 - open Screen bug

---
+++ app/src/main/java/com/example/myapplication1hw/Open_Screen.java
@@ -0,0 +1,19 @@
+package com.example.myapplication1hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Open_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open__screen);
+        findViews();
+
+    }
+
+    private void findViews() {
+    }
+}
